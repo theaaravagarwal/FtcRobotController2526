2ab3fe5515ac1856c4f05e421a4ff375f18ee586
ftcscout <28aagarwal1@stratfordschools.net>
2025-10-08 15:46:19 -0700

    field centric implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FieldCentric.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  * -call init(hardwareMap) from the OpMode init()
  * -call setDrive(strafe, forward, rotation) every loop to drive
  */
-public class RobotCentric {
+public class FieldCentric {
     private DcMotor leftFront, rightFront, leftBack, rightBack;
     //last applied powers
     private double lastFL = 0.0, lastFR = 0.0, lastBL = 0.0, lastBR = 0.0;
@@ -17,7 +17,7 @@ public class RobotCentric {
     private String rightFrontName = "rightFront";
     private String leftBackName = "leftBack";
     private String rightBackName = "rightBack";
-    public RobotCentric() {}
+    public FieldCentric() {}
     public void init(HardwareMap hw) {
         leftFront = hw.get(DcMotor.class, leftFrontName);
         rightFront = hw.get(DcMotor.class, rightFrontName);
@@ -40,26 +40,26 @@ public class RobotCentric {
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
     //drive by: x = strafe (left +, right -), y = forward (forward +, back -), rx = rotation (ccw+)
-    public void setDrive(double x, double y, double rx) {
-        double leftFrontPower = y+x+rx;
-        double leftBackPower = y-x+rx;
-        double rightFrontPower = y-x-rx;
-        double rightBackPower = y+x-rx;
-        double max = Math.max(Math.abs(leftFrontPower), Math.max(Math.abs(leftBackPower), Math.max(Math.abs(rightFrontPower), Math.abs(rightBackPower))));
-        if (max>1.0) {
-            leftFrontPower/=max;
-            leftBackPower/=max;
-            rightFrontPower/=max;
-            rightBackPower/=max;
-        }
-        leftFront.setPower(leftFrontPower);
-        leftBack.setPower(leftBackPower);
-        rightFront.setPower(rightFrontPower);
-        rightBack.setPower(rightBackPower);
-        lastFL = leftFrontPower;
-        lastFR = rightFrontPower;
-        lastBL = leftBackPower;
-        lastBR = rightBackPower;
+    public void setDrive(double x, double y, double rx, double hdRad) {
+        //rot the stick in by the - of the robot dir
+        double c = Math.cos(hdRad);
+        double s = Math.sin(hdRad);
+        double fldX = x*c-y*s;  //rot strafe
+        double fldY = x*s+y*c;  //rot forward
+        //use the rotated inputs for power
+        double lfPow  = fldY+fldX+rx; double lbPow   = fldY-fldX+rx; //left
+        double rfPow = fldY-fldX-rx; double rbPow  = fldY+fldX-rx; //right
+        double max = Math.max(Math.abs(lfPow), Math.max(Math.abs(lbPow), Math.max(Math.abs(rfPow), Math.abs(rbPow)))); //limit values to 1.0
+        if (max>1.0) {lfPow/=max; lbPow/=max; rfPow/=max; rbPow/=max;}
+        //apply the powers
+        leftFront.setPower(lfPow); 
+        leftBack.setPower(lbPow);
+        rightFront.setPower(rfPow); 
+        rightBack.setPower(rbPow);
+        lastFL = lfPow;
+        lastFR = rfPow;
+        lastBL = lbPow;
+        lastBR = rbPow;
     }
     //getters for data
     public double getLastFL() {return lastFL;}
